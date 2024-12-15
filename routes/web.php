116fa32f7575ea9dadcac636435b75573236589f
user.salak96 <lambangsasangka@gmail.com>
2024-12-15 10:57:01 +0700

    css

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,9 @@ use App\Http\Controllers\LaporanController;
 Route::get('/', [LaporanController::class, 'index'])->name('laporans.index');
 Route::get('/laporans/pdf', [LaporanController::class, 'exportPdf'])->name('laporans.pdf');
 Route::get('/laporans/excel', [LaporanController::class, 'exportExcel'])->name('laporans.excel');
-
+// Route::get('/laporans/excel', function () {
+//     return Excel::download(new UsersExport, 'users.xlsx');
+// });
 // Task Routes
 Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
 Route::get('/tasks/{id}', [TaskController::class, 'show'])->name('tasks.show');
